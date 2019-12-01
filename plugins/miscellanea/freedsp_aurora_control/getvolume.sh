32422037cdf4be392117da4273715cb354ec77b9
Bjorn Hijmans <bjorn@hijmans.nl>
2019-12-01 19:50:47 +0000

    WIP

--- plugins/miscellanea/freedsp_aurora_control/getvolume.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo getvolume: Setting SoftMaster to 100%
-
-/usr/bin/amixer -c 5 sset SoftMaster 100%
--- plugins/miscellanea/freedsp_aurora_control/setvolume.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo setvolume: Setting SoftMaster to 100%
-
-/usr/bin/amixer -c 5 sset SoftMaster 100%
-
